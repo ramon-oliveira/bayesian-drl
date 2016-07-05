fddb8e026dad5cdefad67e5820c99cf452545909
Ramon de Oliveira <ramon@roliveira.net>
2016-07-05 11:52:07 -0300

    fix parameters and add terminal flag

--- dqn.py
+++ dqn.py
@@ -16,7 +16,7 @@ def preprocess(I):
 
 
 def populate_memory(env, D, m, k, max_steps):
-    state0_rm, action_rm, reward_rm, state1_rm = D
+    state0_rm, action_rm, reward_rm, state1_rm, terminal_rm = D
     replay_size = state0_rm.shape[0]
 
     n = 0
@@ -38,6 +38,7 @@ def populate_memory(env, D, m, k, max_steps):
             action_rm[m] = action
             reward_rm[n] = reward
             state1_rm[n] = obs1[:]
+            terminal_rm[n] = done
             n += 1
 
             obs0[:] = obs1[:]
@@ -56,39 +57,34 @@ if __name__ == '__main__':
     max_steps = 5000 # maximum number of steps per episode
     size = 80  # image size
     e = 1.0  # e-greedy policy, drops from e=1 to e=0.1
-    k = 4  # the agent sees and selects an action every kth frame
+    k = 2  # the agent sees and selects an action every kth frame
     m = 4  # number of frames looked at each moment
-    replay_size = 50000 # replay memory size
+    replay_size = 100000 # replay memory size
     batch_size = 32 # batch size
-    gama = 0.99  #discount factor for future rewards Q function
-    C = 5000 # frequency target update
+    gamma = 0.99  #discount factor for future rewards Q function
+    C = 10000 # frequency target update
     render = False
 
     # create enviroment
-    env = gym.make('Pong-v0')
+    env = gym.make('Breakout-v0')
 
     # populates replay memory with some random sequences
     state0_rm = np.zeros([replay_size, m, size, size], dtype=np.int8)
     action_rm = np.zeros([replay_size], dtype=np.int8)
     reward_rm = np.zeros([replay_size], dtype=np.int8)
     state1_rm = np.zeros([replay_size, m, size, size], dtype=np.int8)
-    D = [state0_rm, action_rm, reward_rm, state1_rm]
+    terminal_rm = np.zeros([replay_size], dtype=np.bool)
+    D = [state0_rm, action_rm, reward_rm, state1_rm, terminal_rm]
     populate_memory(env, D, m, k, max_steps)
-
-    # batch array
-    state0_batch = np.zeros([replay_size, m, size, size], dtype=np.int8)
-    action_batch = np.zeros([replay_size], dtype=np.int8)
-    reward_batch = np.zeros([replay_size], dtype=np.int8)
-    state1_batch = np.zeros([replay_size, m, size, size], dtype=np.int8)
 
     # Initialize action value function with random with random weights
     print("creating Q network")
     Q = Sequential()
-    Q.add(Convolution2D(32, 8, 8, border_mode='valid', subsample=[4, 4], input_shape=[4, size, size]))
+    Q.add(Convolution2D(32, 8, 8, border_mode='same', subsample=[4, 4], input_shape=[4, size, size]))
     Q.add(Activation('relu'))
-    Q.add(Convolution2D(64, 4, 4, border_mode='valid', subsample=[2, 2]))
+    Q.add(Convolution2D(64, 4, 4, border_mode='same', subsample=[2, 2]))
     Q.add(Activation('relu'))
-    Q.add(Convolution2D(64, 3, 3, border_mode='valid', subsample=[1, 1]))
+    Q.add(Convolution2D(64, 3, 3, border_mode='same', subsample=[1, 1]))
     Q.add(Activation('relu'))
     Q.add(Flatten())
     Q.add(Dense(512, activation='relu'))
@@ -107,8 +103,9 @@ if __name__ == '__main__':
     ##Starts Playing and training
     updates = 0
     idx_rm = 0
+    steps = 0
     idxs_rm = list(range(replay_size))
-    for episodes in range(max_episodes):
+    for episode in range(max_episodes):
         ##Initialize sequence game and sequence s1 pre-process sequence
         obs0 = np.zeros([m, size, size], dtype=np.int8)
         obs1 = np.zeros([m, size, size], dtype=np.int8)
@@ -116,7 +113,6 @@ if __name__ == '__main__':
 
         treward = 0
         pb = Progbar(5000)
-        train_loss = 1e10
 
         action = env.action_space.sample()
         for t in range(max_steps):
@@ -124,33 +120,36 @@ if __name__ == '__main__':
                 if np.random.rand() < e:
                     action = env.action_space.sample()
                 else:
-                    qval = Q.predict(np.array([obs0]), batch_size=1, verbose=0)
+                    qval = Q.predict(np.array([obs0]), verbose=0)
                     action = qval.argmax()
             (ob, reward, done, _info) = env.step(action)
+            steps += 1
 
             # update state
             obs1[1:] = obs1[:m-1]
             obs1[0] = preprocess(ob)
             treward += reward
 
-            # save memory
+            # save replay memory
             state0_rm[idx_rm] = obs0[:]
             action_rm[idx_rm] = action
             reward_rm[idx_rm] = reward
             state1_rm[idx_rm] = obs1[:]
-
+            terminal_rm[idx_rm] = done
 
             # set last state
             obs0[:] = obs1[:]
 
             if (t % k) == 0:
-                ##Sample random minibatch of transitions from D
+                # sample random minibatch of transitions from D
                 idxs = np.random.choice(idxs_rm, size=batch_size)
                 if idx_rm not in idxs:
                     idxs[0] = idx_rm
 
-                y_Q = Q.predict_on_batch(state0_rm[idxs])
-                y_Q_target = reward_rm[idxs] + gama*np.max(Q_target.predict(state1_rm[idxs]), axis=1)
+                y_Q = Q.predict(state0_rm[idxs])
+                maxs = np.max(Q_target.predict(state1_rm[idxs]), axis=1)
+                # maxs = np.max(Q.predict(state1_rm[idxs]), axis=1)
+                y_Q_target = reward_rm[idxs] + (1.0-terminal_rm[idxs])*gamma*maxs
                 y_Q[:, action_rm[idxs]] = y_Q_target
 
                 ##train on batch
@@ -161,11 +160,11 @@ if __name__ == '__main__':
             # update replay idx
             idx_rm = (idx_rm + 1) % replay_size
 
-            ##set e-greedy policy adjust
+            # set e-greedy policy adjust
             if e > 0.1:
-                e -= 0.00009
+                e -= 0.0000009
 
-            ##update Q_target every C trains
+            # update Q_target every C trains
             if (updates % C) == 0:
                 Q_target.set_weights(Q.get_weights())
 
@@ -177,8 +176,10 @@ if __name__ == '__main__':
 
         pb.target = t
         pb.update(t, [['mse', train_loss]], force=True)
-        Q.save_weights('pong.h5', overwrite=True)
-        print("\nEpisode", episodes+1, "\tpoints =", treward, "\tframes",t+1)
+        if (episode % 100) == 0:
+            Q.save_weights('breakout.h5', overwrite=True)
+        print("\nEpisode", episode+1, "\tpoints =", treward, "\ttotal frames", steps,
+              "\te-greedy", e)
         print("")
 
 # if __name__ == '__main__':
