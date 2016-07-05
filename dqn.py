9df69e5cd1d615e326c426a49b01ec419663c951
Ramon de Oliveira <ramon@roliveira.net>
2016-07-04 23:33:54 -0300

    optimize memory replay and observations

--- dqn.py
+++ dqn.py
@@ -5,68 +5,83 @@ from keras.layers.core import Dense, Activation, Flatten
 from keras.layers.convolutional import Convolution2D
 from keras.callbacks import Progbar
 
-def preprocess(obs, size=80):
-    #max_frames = np.maximum.reduce(obs) #quantos frame???
-    I = obs[:, 35:195]
-    I = I[:,::2, ::2, 0]
+
+def preprocess(I):
+    I = I[35:195] # crop score bar
+    I = I[::2, ::2, 0] # down sampling
     I[I == 144] = 0 # erase background (background type 1)
     I[I == 109] = 0 # erase background (background type 2)
     I[I != 0] = 1 # everything else (paddles, ball) just set to
-    #plt.imshow(I, cmap=plt.cm.binary)
-    return I.reshape([1, 4, size, size])
-    # y = np.zeros([len(obs), 110, size])
-    # for i in range(len(obs)):
-    #     y[i] = resize(color.rgb2grey(obs[i]), [110, size])
-    # y = y[:, 13:13+size]
-    # state = y.reshape(1, 4, size, size)
-    # return state/255
+    return I
 
-def populate_memory(env, D, size=5000):
-    n = len(D)
-    while n < size:
-        obs = np.zeros([m]+list(env.observation_space.shape))
-        ob = env.reset()
-        ob0 = ob    #this is for fixing the even odd frames problem
-        obs[0] = ob0
-        State0 = preprocess(obs)
-        treward = 0
-        for i in range(3000):
-            action = env.action_space.sample()
-            (ob1, reward, done, _info) = env.step(action)
-            ob = np.maximum.reduce([ob0,ob1])   #even odd frame
-            ob0 = ob1                           #problem
-            obs[1:m] = obs[0:m-1]
-            obs[0] = ob
-            treward += reward
-            ##Set State' = ob and preprocess State'
-            State1 = preprocess(obs)
-            D.insert(0,[State0,action,reward,State1])
-            State0 = State1
-            if len(D)>replay_size:
-                D.pop()
-            if done:break;
-            n = len(D)
-            if(len(D)%100==0):
-                print("Replay memory lenght",len(D))
-            if n > size: break
-    return n
 
-def act(env, obs):
-    return env.action_space.sample()
+def populate_memory(env, D, m, k, max_steps):
+    state0_rm, action_rm, reward_rm, state1_rm = D
+    replay_size = state0_rm.shape[0]
+
+    n = 0
+    obs0 = np.zeros([m, 80, 80], dtype=np.int8)
+    obs1 = np.zeros([m, 80, 80], dtype=np.int8)
+    while n < replay_size:
+        obs0[:] = obs1[:] = preprocess(env.reset())
+        action = env.action_space.sample()
+        for i in range(max_steps):
+            if i % k == 0:
+                action = env.action_space.sample()
+            (ob, reward, done, _info) = env.step(action)
+
+            obs1[1:] = obs0[:m-1]
+            obs1[0] = preprocess(ob)
+
+            # save memory
+            state0_rm[n] = obs0[:]
+            action_rm[m] = action
+            reward_rm[n] = reward
+            state1_rm[n] = obs1[:]
+            n += 1
+
+            obs0[:] = obs1[:]
+
+            if (n % 1000) == 0:
+                print('Replay memory lenght', n)
+            if done or n >= replay_size:
+                break
+
 
 if __name__ == '__main__':
 
+    # parameters
+    total_frames = 10000000
+    max_episodes = 10000
+    max_steps = 5000 # maximum number of steps per episode
+    size = 80  # image size
+    e = 1.0  # e-greedy policy, drops from e=1 to e=0.1
+    k = 4  # the agent sees and selects an action every kth frame
+    m = 4  # number of frames looked at each moment
+    replay_size = 50000 # replay memory size
+    batch_size = 32 # batch size
+    gama = 0.99  #discount factor for future rewards Q function
+    C = 5000 # frequency target update
+    render = False
+
+    # create enviroment
     env = gym.make('Pong-v0')
 
-    Total_Frames = 50000000
-    Max_ep = 1000000
-    num_steps = 5000
-    size = 80
-    #Lets assume the following parameters are the same
+    # populates replay memory with some random sequences
+    state0_rm = np.zeros([replay_size, m, size, size], dtype=np.int8)
+    action_rm = np.zeros([replay_size], dtype=np.int8)
+    reward_rm = np.zeros([replay_size], dtype=np.int8)
+    state1_rm = np.zeros([replay_size, m, size, size], dtype=np.int8)
+    D = [state0_rm, action_rm, reward_rm, state1_rm]
+    populate_memory(env, D, m, k, max_steps)
 
-    ##Initialize replay memory D to capacity N (1000000)
-    D = list()  ###### TODO: use numpy array
-    ##Initialize action value function with random with random weights
+    # batch array
+    state0_batch = np.zeros([replay_size, m, size, size], dtype=np.int8)
+    action_batch = np.zeros([replay_size], dtype=np.int8)
+    reward_batch = np.zeros([replay_size], dtype=np.int8)
+    state1_batch = np.zeros([replay_size, m, size, size], dtype=np.int8)
+
+    # Initialize action value function with random with random weights
     print("creating Q network")
     Q = Sequential()
     Q.add(Convolution2D(32, 8, 8, border_mode='valid', subsample=[4, 4], input_shape=[4, size, size]))
@@ -83,91 +98,75 @@ if __name__ == '__main__':
     Q.compile(loss="mse", optimizer='adadelta')
     Q.summary()
 
-    ##Initialize target action-value function ^Q with same wieghts
+    # initialize target action-value function ^Q with same wieghts
     print("copying Q to Q_target")
     Q_target = model_from_yaml(Q.to_yaml())
     Q_target.set_weights(Q.get_weights())
 
-    e = 1.0 #e-greedy policy, drops from e=1 to e=0.1
-    k = 4 #The agent sees and selects an action every kth frame
-    m = 4 #Number of frames looked at each moment
-    replay_size = 10000
-    batch_size = 32
-    gama = 0.99  #discount factor for future rewards Q function
-    C = 5000
-    render = False
-
-    ##Populates replay memory with some random sequences
-    print("populating memory")
-    populate_memory(env, D, size=5000)
-
-    S0 = np.zeros([batch_size, m, size, size])
-    S1 = np.zeros([batch_size, m, size, size])
-    A = np.zeros([batch_size], dtype=np.int)
-    R = np.zeros([batch_size])
-
     print("Starting to Train")
     ##Starts Playing and training
     updates = 0
-    for episodes in range(Max_ep):
+    idx_rm = 0
+    idxs_rm = list(range(replay_size))
+    for episodes in range(max_episodes):
         ##Initialize sequence game and sequence s1 pre-process sequence
-        obs = np.zeros([m]+list(env.observation_space.shape))
-        ob = env.reset()
-        ob0 = ob    #this is for fixing the even odd frames problem
-        obs[0] = ob0
-        State0 = preprocess(obs)
+        obs0 = np.zeros([m, size, size], dtype=np.int8)
+        obs1 = np.zeros([m, size, size], dtype=np.int8)
+        obs0[:] = obs1[:] = preprocess(env.reset())
+
         treward = 0
         pb = Progbar(5000)
         train_loss = 1e10
-        for t in range(num_steps):
-            if t%k==0:
+
+        action = env.action_space.sample()
+        for t in range(max_steps):
+            if (t % k) == 0:
                 if np.random.rand() < e:
                     action = env.action_space.sample()
                 else:
-                    in_obs = preprocess(obs)
-                    qval = Q.predict(in_obs, batch_size=1, verbose=0)
+                    qval = Q.predict(np.array([obs0]), batch_size=1, verbose=0)
                     action = qval.argmax()
-            (ob1, reward, done, _info) = env.step(action)
-            ob = np.maximum.reduce([ob0,ob1])   #even odd frame
-            ob0 = ob1                           #problem
-            obs[1:m] = obs[0:m-1]
-            obs[0] = ob
+            (ob, reward, done, _info) = env.step(action)
+
+            # update state
+            obs1[1:] = obs1[:m-1]
+            obs1[0] = preprocess(ob)
             treward += reward
 
-            ##Set State' = ob and preprocess State'
-            State1 = preprocess(obs)
+            # save memory
+            state0_rm[idx_rm] = obs0[:]
+            action_rm[idx_rm] = action
+            reward_rm[idx_rm] = reward
+            state1_rm[idx_rm] = obs1[:]
 
-            ##### TODO: numpy array
-            D.insert(0,[State0,action,reward,State1])
-            if len(D)>replay_size:
-                D.pop()
-            State0 = State1
 
-            ##train only every few actions
-            if t%4==0:
+            # set last state
+            obs0[:] = obs1[:]
+
+            if (t % k) == 0:
                 ##Sample random minibatch of transitions from D
-                batch = [D[i] for i in sorted(np.random.randint(0,len(D),32))]
-                for i, (s0, a, r, s1) in enumerate(batch):
-                    S0[i] = s0
-                    S1[i] = s1
-                    A[i] = a
-                    R[i] = r
-                y_Q = Q.predict_on_batch(S0)
-                y_Q_target = R +gama*np.max(Q_target.predict_on_batch(S1),1)
-                #y_Q_target = R + gama*np.max(Q.predict_on_batch(S1), 1)
-                y_Q[:, A] = y_Q_target
+                idxs = np.random.choice(idxs_rm, size=batch_size)
+                if idx_rm not in idxs:
+                    idxs[0] = idx_rm
+
+                y_Q = Q.predict_on_batch(state0_rm[idxs])
+                y_Q_target = reward_rm[idxs] + gama*np.max(Q_target.predict(state1_rm[idxs]), axis=1)
+                y_Q[:, action_rm[idxs]] = y_Q_target
 
                 ##train on batch
-                train_loss = Q.train_on_batch(S0,y_Q)
+                train_loss = Q.train_on_batch(state0_rm[idxs], y_Q)
                 updates += 1
                 pb.update(t, [['mse', train_loss]])
+
+            # update replay idx
+            idx_rm = (idx_rm + 1) % replay_size
 
             ##set e-greedy policy adjust
             if e > 0.1:
                 e -= 0.00009
 
             ##update Q_target every C trains
-            if updates%C==0:
+            if (updates % C) == 0:
                 Q_target.set_weights(Q.get_weights())
 
             if render:
@@ -182,4 +181,5 @@ if __name__ == '__main__':
         print("\nEpisode", episodes+1, "\tpoints =", treward, "\tframes",t+1)
         print("")
 
-
+# if __name__ == '__main__':
+#     train()
